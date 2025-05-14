46ed09a52090bd48b8df5cb985e773582191b573
faizm10 <faizmustansar10@gmail.com>
2025-05-13 21:04:25 -0400

    removed eslint

--- frontend/app/browse/page.tsx
+++ frontend/app/browse/page.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import Link from "next/link";
-import Image from "next/image";
 import { Header } from "@/components/header";
 import MinimalShop from "@/components/kokonutui/minimal-shop";
 
